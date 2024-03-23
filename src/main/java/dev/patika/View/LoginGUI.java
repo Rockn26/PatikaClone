8030d236ceb71113b29d27b825d72a8fb09bc9bf
Rockn26 <irfanank26@gmail.com>
2024-03-23 20:24:07 +0300

    Create GUI

---
+++ src/main/java/dev/patika/View/LoginGUI.java
@@ -0,0 +1,66 @@
+package dev.patika.View;
+
+import dev.patika.Helper.Config;
+import dev.patika.Helper.Helper;
+import dev.patika.Model.Educator;
+import dev.patika.Model.Operator;
+import dev.patika.Model.Student;
+import dev.patika.Model.User;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginGUI extends JFrame{
+    private JPanel wrapper;
+    private JPanel wbottom;
+    private JPanel wtop;
+    private JTextField fld_user_uname;
+    private JPasswordField fld_user_pass;
+    private JButton btn_login;
+    private JButton btn_register;
+
+    public LoginGUI(){
+        add(wrapper);
+        setSize(400, 400);
+        setLocation(Helper.scCenter("x", getSize()), Helper.scCenter("y", getSize()));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle(Config.PROJECT_TITLE);
+        setResizable(false);
+        setVisible(true);
+
+        btn_login.addActionListener(e -> {
+            if (Helper.isFieldEmpty(fld_user_uname) || Helper.isFieldEmpty(fld_user_pass)){
+                Helper.showMsg("fill");
+            }else{
+                User u = User.getFetch(fld_user_uname.getText(), fld_user_pass.getText());
+                if (u == null){
+                    Helper.showMsg("Kullanıcı Bulunamadı !");
+                } else {
+                    switch (u.getType()){
+                        case "operator":
+                            OperatorGUI opGUI = new OperatorGUI((Operator) u);
+                            break;
+                        case "educator":
+                            EducatorGUI edGUI = new EducatorGUI((Educator) u);
+                            break;
+                        case "student":
+                            StudentGUI stGUI = new StudentGUI((Student) u);
+                            break;
+                    }
+                    dispose();
+                }
+            }
+        });
+
+        btn_register.addActionListener(e -> {
+            SignInGUI signInGui = new SignInGUI();
+            dispose();
+        });
+    }
+
+    public static void main(String[] args) {
+        Helper.setLayout();
+        LoginGUI login = new LoginGUI();
+    }
+}
